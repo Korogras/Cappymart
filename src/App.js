0c332e32d0cea0fc8c0ff7eda3936e2590837259
Korogras <brandon_faubion@yahoo.com>
2023-10-02 17:26:19 -0500

    added cart

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/products/:id" element={<Product />} />
         <Route path="/products" element={<Products />} />
         <Route path='/categories/:name' element={<CategoryProducts/>} />
-        <Route path='cart' element={<Cart/>}/>
+        <Route path='/cart' element={<Cart/>}/>
         <Route path="*" element={<div>404</div>} />
       </Routes>
       <Footer />
